2b500623eb717c55ed48283bd92944e7dc19ef90
vinnie <mymailaddress010020@gmail.com>
2023-01-07 00:14:15 +0630

    final coding

--- assets/app.js
+++ assets/app.js
@@ -1,89 +1,67 @@
-let date = new Date();
+function updateDynamic() {
+  let date = new Date();
 
-// FOR TIME
-let h;
-let m = date.getMinutes();
-let s = date.getSeconds();
-let ms = date.getMilliseconds();
-let ampm;
+  // nc -> nc means not calculated
+  let nc_hour = date.getHours();
+  let nc_minute = date.getMinutes();
+  let nc_second = date.getSeconds();
 
-// FOR DATE
-let currentDate;
-let year = date.getFullYear();
-let month_arr = [
-  "Jan",
-  "Feb",
-  "March",
-  "April",
-  "May",
-  "June",
-  "July",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-let month = month_arr[date.getMonth()];
-let day_arr = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let weekDay = day_arr[date.getDay()];
+  // FOR TIME
+  let h;
+  let m = nc_minute < 10 ? "0" + nc_minute : nc_minute;
+  let s = nc_second < 10 ? "0" + nc_second : nc_second;
+  let ampm = nc_hour >= 12 ? "AM" : "PM";
 
-// nc -> nc means not calculated
-let nc_hour = date.getHours();
-let nc_minute = date.getMinutes();
-let nc_second = date.getSeconds();
-// am pm
-if (nc_hour >= 12) {
-  ampm = "PM";
-} else {
-  ampm = "Am";
-}
-
-// hour
-if (nc_hour > 12) {
-  let realHr = nc_hour - 12;
-  h = "0" + realHr;
-  if (h < 10) {
-    h = "0" + nc_hour;
+  // hour
+  if (nc_hour > 12) {
+    let realHr = nc_hour - 12;
+    h = "0" + realHr;
+    if (h < 10) {
+      h = "0" + nc_hour;
+    } else {
+      h = nc_hour - 12;
+    }
+  } else if (nc_hour === 0) {
+    h = 12;
+  } else {
+    hr = "0" + nc_hour;
   }
-} else if (nc_hour === 0) {
-  h = 12;
-} else {
-  hr = "0" + nc_hour;
+
+  // FOR DATE
+  let currentDate = date.getDate() < 10 ? "0" + date.getDate() : date.getDate();
+  let year = date.getFullYear();
+  let month_arr = [
+    "Jan",
+    "Feb",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let month = month_arr[date.getMonth()];
+  let day_arr = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let weekDay = day_arr[date.getDay()];
+
+  let timeDiv = document.querySelector(".hms");
+  timeDiv.innerHTML = `<p> ${h} : ${m} : ${s} <span>${ampm}</span></p>`;
+
+  document.querySelector(
+    ".dmy"
+  ).innerHTML = `<p>${currentDate} ${month} ${year} ${weekDay}</p>`;
 }
 
-// minute
-if (nc_minute < 10) {
-  m = "0" + nc_minute;
-} else {
-  m = nc_minute;
-}
-
-// sec
-if (nc_second < 10) {
-  s = "0" + nc_second;
-} else {
-  s = nc_second;
-}
-
-// date
-if (date.getDate() < 10) {
-  currentDate = "0" + date.getDate();
-} else {
-  currentDate = date.getDate();
-}
-
-let timeDiv = document.querySelector(".hms");
-timeDiv.innerHTML = `<p> ${h} : ${m} : ${s} <span>${ampm}</span></p>`;
-
-document.querySelector(
-  ".dmy"
-).innerHTML = `<p>${currentDate} ${month} ${year} ${weekDay}</p>`;
+setInterval(updateDynamic, 1000);
